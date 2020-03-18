af0dc69da2f08c80d238fd079bc811e6a601f83e
standash <stdashevsky@gmail.com>
2020-03-18 15:22:14 +0100

    Finally got ACK messages to work...

--- sock.py
+++ sock.py
@@ -5,16 +5,21 @@ import socket
 import xml.etree.ElementTree as ET
 from datetime import datetime
 from dateutil.relativedelta import *
+import xml.dom.minidom
 
 poc_host, poc_port = "192.168.2.100", 3001
 
+def prettyprint(bytez):
+    s = bytez.decode("utf-8")
+    dom = xml.dom.minidom.parseString(s)
+    print(dom.toprettyxml())
 
 def increment_timestamp(timestamp, n_seconds):
     chunks = timestamp.split("-")
     xxx = chunks[2].split("T")
     time = datetime.strptime(xxx[1], "%H:%M:%S")
     time = time + relativedelta(seconds = n_seconds)
-    return "{}-{}-{}T{}-{}".format(chunks[0], chunks[1], xxx[0], "{}:{}:{}".format(time.hour, time.minute, time.second), chunks[3])
+    return "{}-{}-{}T{}-{}".format(chunks[0], chunks[1], xxx[0], time.strftime("%H:%M:%S"), chunks[3])
 
 
 def parse_xml(xml_element, template):
@@ -40,11 +45,6 @@ def parse_hello_message(message):
     return msg
 
 def generate_ack_message(msg):
-    # x_bytez = b""
-    # with open ("ack_binary", "rb") as _f:
-        # x_bytez = _f.read()
-    # print(x_bytez.decode("utf-8"))
-    ########################################################
     bytez = b""
     bytez += "<?xml version=\"1.0\" encoding=\"utf-8\"?>".encode("utf-8")
 
@@ -68,13 +68,11 @@ def generate_ack_message(msg):
 
     bytez += "<ACK.type_cd V=\"AA\"/>".encode("utf-8")
 
-    bytez += "<ACK.control_id V=\"{}\" />".format(msg["control_id"]).encode("utf-8")
+    bytez += "<ACK.ack_control_id V=\"{}\" />".format(msg["control_id"]).encode("utf-8")
 
     bytez += "</ACK>".encode("utf-8")
 
     bytez += "</ACK.R01>".encode("utf-8")
-    ########################################################
-    # print(bytez.decode("utf-8"))
 
     return bytez
 
@@ -94,16 +92,21 @@ if __name__ == "__main__":
                     break
                 data_str = data.strip().decode("utf-8")
                 if "HEL.R01" in data_str:
-                    print("DCA => [HELLO]")
+                    print("DCA => [HELLO]\n")
+                    prettyprint(data)
+
                     hello_msg = parse_hello_message(data_str)
                     ack_bytes = generate_ack_message(hello_msg)
                     conn.send(ack_bytes)
-                    print("[ACK] => DCA")
+
+                    print("[ACK] => DCA\n")
+                    prettyprint(ack_bytes)
                 elif "ESC.R01" in data_str and "OTH" in data_str:
                     print("!!!BAD ACK!!!")
                     # raise Exception("!!!BAD ACK!!!")
                 else:
-                    print(data)
+                    print("DCA => [???]\n")
+                    prettyprint(data)
             sock.close()
     except Exception as e:
         print("ERROR: {}".format(e))
